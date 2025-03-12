085baad51104eb5309c9f0ed7c1c98531c7f0121
bigfoodk <satcom1600@hanmail.net>
2025-03-12 18:12:08 +0900

    Install material ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,23 @@
+import {
+  AppBar,
+  CssBaseline,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import createThemeWithUserColorScheme from "./util/createThemeWithUserColorScheme";
+
+const theme = createThemeWithUserColorScheme();
+
 export default function App() {
   return (
-    <>
-      <p>hello world</p>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">QWERTY</Typography>
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
   );
 }
