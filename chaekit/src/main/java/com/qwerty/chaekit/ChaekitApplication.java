7bfce56368d7a84aad8272ca06f275a068e91eb7
고종환 <jonghwankoh@ajou.ac.kr>
2025-03-21 14:32:51 +0900

    init

---
+++ chaekit/src/main/java/com/qwerty/chaekit/ChaekitApplication.java
@@ -0,0 +1,13 @@
+package com.qwerty.chaekit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaekitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaekitApplication.class, args);
+	}
+
+}
---
+++ chaekit/src/test/java/com/qwerty/chaekit/ChaekitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qwerty.chaekit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaekitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
