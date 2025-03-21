e4337e4f02d2221ab31227ba90e7a34a7405f794
bigfoodk <satcom1600@hanmail.net>
2025-03-21 19:34:25 +0900

    Install router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
-import {
-  AppBar,
-  CssBaseline,
-  ThemeProvider,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import createThemeWithUserColorScheme from "./util/createThemeWithUserColorScheme";
+import { routeTree } from "./routeTree.gen";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+
+const router = createRouter({ routeTree });
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 const theme = createThemeWithUserColorScheme();
 
@@ -13,11 +16,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">QWERTY</Typography>
-        </Toolbar>
-      </AppBar>
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
