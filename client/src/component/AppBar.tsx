fa472507a81c71658e4d99aceb1224bc156ac290
bigfoodk <satcom1600@hanmail.net>
2025-03-26 15:49:57 +0900

    Add login button on app bar

--- client/src/component/AppBar.tsx
+++ client/src/component/AppBar.tsx
@@ -1,15 +1,26 @@
-import { Toolbar, Typography, AppBar as MuiAppBar } from "@mui/material";
-import { Outlet } from "react-router";
+import {
+  Toolbar,
+  Typography,
+  AppBar as MuiAppBar,
+  Button,
+} from "@mui/material";
+import { Link } from "@tanstack/react-router";
 
 export default function AppBar() {
   return (
-    <>
-      <MuiAppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">QWERTY</Typography>
-        </Toolbar>
-      </MuiAppBar>
-      <Outlet />
-    </>
+    <MuiAppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" sx={{ mr: "auto" }}>
+          QWERTY
+        </Typography>
+        <Link to="/login">
+          {({ isActive }) => {
+            return (
+              <Button variant={isActive ? "contained" : "text"}>로그인</Button>
+            );
+          }}
+        </Link>
+      </Toolbar>
+    </MuiAppBar>
   );
 }
