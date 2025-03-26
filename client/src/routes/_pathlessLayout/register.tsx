494e6803c1f9774c18a9f6d1a60dca6775c8e581
bigfoodk <satcom1600@hanmail.net>
2025-03-26 17:40:13 +0900

    Call join api

--- client/src/routes/_pathlessLayout/register.tsx
+++ client/src/routes/_pathlessLayout/register.tsx
@@ -10,6 +10,8 @@ import {
   useTheme,
 } from "@mui/material";
 import { createFileRoute } from "@tanstack/react-router";
+import useJoin from "../../api/join/useJoin";
+import { useCallback, useState } from "react";
 
 export const Route = createFileRoute("/_pathlessLayout/register")({
   component: RouteComponent,
@@ -17,6 +19,24 @@ export const Route = createFileRoute("/_pathlessLayout/register")({
 
 function RouteComponent() {
   const theme = useTheme();
+  const { join } = useJoin();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const onRegisterButtonClick = useCallback(() => {
+    // TODO: Test username and password constraints
+
+    if (password !== confirmPassword) {
+      alert("비밀번호가 일치하지 않습니다.");
+      return;
+    }
+
+    join({
+      username,
+      password,
+    });
+  }, [join, username, password, confirmPassword]);
 
   return (
     <Container maxWidth="sm" sx={{ mt: theme.spacing(4) }}>
@@ -30,21 +50,34 @@ function RouteComponent() {
           }}
         >
           <InputLabel>ID</InputLabel>
-          <OutlinedInput placeholder="ID" fullWidth />
-
-          <Divider />
-
-          <InputLabel>Password</InputLabel>
-          <OutlinedInput placeholder="Password" fullWidth type="password" />
           <OutlinedInput
-            placeholder="Confirm Password"
+            placeholder="ID"
             fullWidth
-            type="password"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
 
           <Divider />
 
-          <Button fullWidth variant="contained">
+          <InputLabel>Password</InputLabel>
+          <OutlinedInput
+            placeholder="Password"
+            fullWidth
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <OutlinedInput
+            placeholder="Confirm Password"
+            fullWidth
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+
+          <Divider />
+
+          <Button fullWidth variant="contained" onClick={onRegisterButtonClick}>
             가입하기
           </Button>
           {/* TODO: Add more fields */}
