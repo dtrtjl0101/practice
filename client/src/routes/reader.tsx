aff883b11dccf6aed5459755d8b0b680bcf3cb42
bigfoodk <satcom1600@hanmail.net>
2025-03-27 16:05:51 +0900

    Highlight text and create memo on selecting text

--- client/src/routes/reader.tsx
+++ client/src/routes/reader.tsx
@@ -2,7 +2,7 @@ import { Box, Button, useTheme } from "@mui/material";
 import { createFileRoute } from "@tanstack/react-router";
 import { EpubCFI, Rendition } from "epubjs";
 import { nanoid } from "nanoid";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { ReactReader } from "react-reader";
 
 export const Route = createFileRoute("/reader")({
@@ -13,13 +13,19 @@ type Memo = {
   id: string;
   cfiRange: string;
 };
+type MemoDiff = {
+  added: Memo[];
+  removed: Memo[];
+};
 
 function RouteComponent() {
   const theme = useTheme();
   const [location, setLocation] = useState<string | number>(10);
   const [memos, setMemos] = useState<Memo[]>([]);
+  const [memosInPage, setMemosInPage] = useState<Memo[]>([]);
   const [epubUrl, setEpubUrl] = useState<ArrayBuffer>(new ArrayBuffer());
   const [rendition, setRendition] = useState<Rendition | undefined>(undefined);
+  const previousMemosInPage = useRef<Memo[]>([]);
 
   const uploadEpub = useCallback(() => {
     let inputElement = document.createElement("input");
@@ -37,13 +43,46 @@ function RouteComponent() {
     inputElement.click();
   }, [setEpubUrl]);
 
-  const memosInPage = useMemo(() => {
-    if (!rendition) {
-      return [];
+  useEffect(() => {
+    previousMemosInPage.current = memosInPage;
+    if (!rendition || !rendition.location) {
+      setMemosInPage([]);
+      return;
     }
+
     const cfi = new EpubCFI();
-    const 
-  }, [memos, rendition, location]);
+    const startCfi = rendition.location.start.cfi;
+    const endCfi = rendition.location.end.cfi;
+    const newMemosInPage = memos.filter((memo) => {
+      if (cfi.compare(startCfi, memo.cfiRange) >= 0) {
+        return false;
+      }
+      if (cfi.compare(memo.cfiRange, endCfi) >= 0) {
+        return false;
+      }
+      return true;
+    });
+
+    setMemosInPage(newMemosInPage);
+    return;
+  }, [memos, rendition, location, setMemosInPage]);
+
+  useEffect(() => {
+    if (!rendition) {
+      return;
+    }
+
+    const { added, removed } = diffMemos(
+      previousMemosInPage.current,
+      memosInPage
+    );
+    added.forEach((memo) => {
+      rendition.annotations.highlight(memo.cfiRange, {}, () => {});
+    });
+    removed.forEach((memo) => {
+      rendition.annotations.remove(memo.cfiRange, "highlight");
+    });
+  }, [rendition, memosInPage]);
 
   return (
     <Box
@@ -70,22 +109,31 @@ function RouteComponent() {
         }}
         location={location}
         locationChanged={(epubcfi: string) => {
-          const qwe = new EpubCFI(epubcfi);
           setLocation(epubcfi);
-          if (rendition) {
-            console.log(
-              rendition.location.start.cfi,
-              rendition.location.end.cfi,
-            );
-          }
         }}
         handleTextSelected={(cfiRange, _contents) => {
-          // setMemos((prev) => [...prev, { id: nanoid(), cfiRange }]);
-          console.log(cfiRange);
+          // TODO: Add memo with button
+          setMemos((prev) => [...prev, { id: nanoid(), cfiRange }]);
         }}
         showToc={false}
-        getRendition={(rendition) => setRendition(rendition)}
+        getRendition={(newRendition) => {
+          setRendition(newRendition);
+        }}
       />
     </Box>
   );
+}
+
+function diffMemos(prev: Memo[], next: Memo[]): MemoDiff {
+  const added = next.filter(
+    (memo) => !prev.some((prevMemo) => prevMemo.id === memo.id)
+  );
+  const removed = prev.filter(
+    (memo) => !next.some((nextMemo) => nextMemo.id === memo.id)
+  );
+
+  return {
+    added,
+    removed,
+  };
 }
