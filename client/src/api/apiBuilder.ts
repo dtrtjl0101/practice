d3760a6b0a41ddd1ac4353350af116608a9ee068
bigfoodk <satcom1600@hanmail.net>
2025-03-29 12:58:53 +0900

    Update api spec

--- client/src/api/apiBuilder.ts
+++ client/src/api/apiBuilder.ts
@@ -1,4 +1,5 @@
 import { ENV } from "../env";
+import { Role } from "../types/role";
 
 export type ApiMethod = "GET" | "POST" | "PUT" | "DELETE";
 export type BaseResponseBody<D, E = string> =
@@ -12,42 +13,90 @@ export type BaseResponseBody<D, E = string> =
     };
 
 type ApiSpec = {
-  join: {
-    request: {
-      username: string;
-      password: string;
-    };
-    response: BaseResponseBody<
-      {
-        authToken: string;
-      },
-      string
-    >;
-  };
   login: {
-    request: {
-      asd: string;
+    POST: {
+      request: {
+        username: string;
+        password: string;
+      };
+      response: BaseResponseBody<
+        {
+          id: number;
+          nickname: string;
+          username: string;
+          role: Role;
+        },
+        undefined
+      >;
     };
-    response: BaseResponseBody<undefined, string>;
+    GET: {
+      request: undefined;
+      response: undefined;
+    };
+    PUT: {
+      request: undefined;
+      response: undefined;
+    };
+    DELETE: {
+      request: undefined;
+      response: undefined;
+    };
+  };
+  "users/join": {
+    POST: {
+      request: {
+        nickname: string;
+        username: string;
+        password: string;
+      };
+      response: BaseResponseBody<
+        {
+          nickname: string;
+          username: string;
+          role: Role;
+        },
+        undefined
+      >;
+    };
+    GET: {
+      request: undefined;
+      response: undefined;
+    };
+    PUT: {
+      request: undefined;
+      response: undefined;
+    };
+    DELETE: {
+      request: undefined;
+      response: undefined;
+    };
   };
 };
 
 export type ApiPath = keyof ApiSpec;
-export type ApiRequestBody<Path extends ApiPath> = ApiSpec[Path]["request"];
-export type ApiResponseBody<Path extends ApiPath> = ApiSpec[Path]["response"];
+export type ApiRequestBody<
+  Method extends ApiMethod,
+  Path extends ApiPath,
+> = ApiSpec[Path][Method]["request"];
+export type ApiResponseBody<
+  Method extends ApiMethod,
+  Path extends ApiPath,
+> = ApiSpec[Path][Method]["response"];
 
 export class ApiBuilder<
+  Method extends ApiMethod,
   Path extends ApiPath,
-  Body = ApiRequestBody<Path>,
-  Response_ = ApiResponseBody<Path>,
+  Body = ApiRequestBody<Method, Path>,
+  Response_ = ApiResponseBody<Method, Path>,
 > {
   private _authToken: string | undefined = undefined;
   private _path: string = "";
   private _method: ApiMethod = "GET";
   private _body: Body | undefined = undefined;
 
-  constructor(path: ApiPath) {
+  constructor(method: ApiMethod, path: ApiPath) {
     this._path = path;
+    this._method = method;
   }
 
   public authToken(authToken: string) {
@@ -55,18 +104,17 @@ export class ApiBuilder<
     return this;
   }
 
-  public method(method: ApiMethod) {
-    this._method = method;
-    return this;
-  }
-
   public body(body: Body) {
+    if (this._method === "GET") {
+      throw new Error("Cannot set body for GET request");
+    }
+
     this._body = body;
     return this;
   }
 
   public async send() {
-    const response = await fetch(`${ENV.CHAEKIT_API_ENDPOINT}/${this._path}`, {
+    const response = await fetch(`${ENV.CHAEKIT_API_ENDPOINT}${this._path}`, {
       method: this._method,
       headers: {
         "Content-Type": "application/json",
