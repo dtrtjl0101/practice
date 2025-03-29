30e49d4a98a235e59b4a9601ae2a381cd888138c
bigfoodk <satcom1600@hanmail.net>
2025-03-29 15:21:03 +0900

    Use Role instead string

--- client/src/states/auth.ts
+++ client/src/states/auth.ts
@@ -1,11 +1,12 @@
 import { atom } from "jotai";
+import { Role } from "../types/role";
 
 export namespace AuthState {
   export type LoggedInUser = {
     id: number;
     nickname: string;
     username: string;
-    role: string;
+    role: Role;
   };
   export const user = atom<LoggedInUser | undefined>();
 }
