680315acbb3176f1df3dbc05fde84317dd2ffe25
bigfoodk <satcom1600@hanmail.net>
2025-04-03 18:15:08 +0900

    Show admin button on appbar if logged in as admin

--- client/src/component/AppBar.tsx
+++ client/src/component/AppBar.tsx
@@ -5,18 +5,34 @@ import {
   Button,
 } from "@mui/material";
 import { Link } from "@tanstack/react-router";
+import { useAtomValue } from "jotai";
+import { AuthState } from "../states/auth";
+import { Role } from "../types/role";
 
 export default function AppBar() {
+  const user = useAtomValue(AuthState.user);
+
   return (
     <MuiAppBar position="static">
       <Toolbar>
         <Typography variant="h6" sx={{ mr: "auto" }}>
           QWERTY
         </Typography>
+        {user && user.role === Role.ROLE_ADMIN && (
+          <Link to="/admin">
+            {({ isActive }) => {
+              return (
+                <Button color={isActive ? "primary" : "secondary"}>
+                  관리자
+                </Button>
+              );
+            }}
+          </Link>
+        )}
         <Link to="/login">
           {({ isActive }) => {
             return (
-              <Button variant={isActive ? "contained" : "text"}>로그인</Button>
+              <Button color={isActive ? "primary" : "secondary"}>로그인</Button>
             );
           }}
         </Link>
