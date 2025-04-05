c80d77f12309e8ee4ec61f430bc69d3bc03e032a
고종환 <jonghwankoh@ajou.ac.kr>
2025-04-05 23:12:37 +0900

    docs: login api docs 추가

---
+++ chaekit-spring/src/main/java/qwerty/chaekit/global/config/swagger/LoginDocsController.java
@@ -0,0 +1,21 @@
+package qwerty.chaekit.global.config.swagger;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import qwerty.chaekit.dto.LoginRequest;
+import qwerty.chaekit.dto.LoginResponse;
+
+@RestController(value = "LoginController")
+@RequestMapping("/api/login")
+@Tag(name = "login-filter", description = "로그인 API")
+public class LoginDocsController {
+    @Operation(summary = "로그인", description = "Spring Security가 처리하는 로그인 API")
+    @PostMapping
+    public LoginResponse login(@RequestBody LoginRequest loginRequest) {
+        return null;
+    }
+}
