8fd450eb61d7db640e54c2bb5e7e7b0e58980fce
bigfoodk <satcom1600@hanmail.net>
2025-04-07 00:32:35 +0900

    Add group create form

--- client/src/routes/_pathlessLayout/mypage/groups.tsx
+++ client/src/routes/_pathlessLayout/mypage/groups.tsx
@@ -1,17 +1,50 @@
-import { Card, CardContent, CardHeader, Typography } from "@mui/material";
+import { GroupAdd } from "@mui/icons-material";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  IconButton,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { createFileRoute } from "@tanstack/react-router";
+import GroupCreateModal from "../../../component/groupCreate/GroupCreateModal";
+import { useState } from "react";
 
 export const Route = createFileRoute("/_pathlessLayout/mypage/groups")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const [openGroupCreateModal, setOpenGroupCreateModal] = useState(false);
+
   return (
-    <Card>
-      <CardHeader title="Groups" />
-      <CardContent>
-        <Typography>This is the groups page.</Typography>
-      </CardContent>
-    </Card>
+    <>
+      <GroupCreateModal
+        open={openGroupCreateModal}
+        onClose={() => setOpenGroupCreateModal(false)}
+      />
+      <Stack spacing={2}>
+        <Card>
+          <CardHeader
+            title="내 그룹"
+            action={
+              <IconButton onClick={() => setOpenGroupCreateModal(true)}>
+                <GroupAdd />
+              </IconButton>
+            }
+          />
+          <CardContent>
+            <Typography>[여기에 그룹 표시]</Typography>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader title="내가 속한 그룹" />
+          <CardContent>
+            <Typography>[여기에 그룹 표시]</Typography>
+          </CardContent>
+        </Card>
+      </Stack>
+    </>
   );
 }
