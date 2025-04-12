b48f04fcf93eebed87f1dcf5ba6f843069c117f0
bigfoodk <satcom1600@hanmail.net>
2025-04-12 16:00:35 +0900

    Use create group api

--- client/src/component/groupList.tsx
+++ client/src/component/groupList.tsx
@@ -93,8 +93,8 @@ function ItemContainer(props: PropsWithChildren) {
         xs: 12,
         sm: 6,
         md: 4,
-        lg: 3,
-        xl: 2,
+        lg: 4,
+        xl: 4,
       }}
     >
       {children}
