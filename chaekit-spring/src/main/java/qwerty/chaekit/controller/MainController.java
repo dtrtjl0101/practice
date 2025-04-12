8f638b1ffb52971109b3008f16b94f22bb55ef6e
jonghwankoh <66508805+jonghwankoh@users.noreply.github.com>
2025-04-12 15:03:10 +0900

    feat/be/entity (#122)

--- chaekit-spring/src/main/java/qwerty/chaekit/controller/MainController.java
+++ chaekit-spring/src/main/java/qwerty/chaekit/controller/MainController.java
@@ -1,13 +1,16 @@
 package qwerty.chaekit.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import qwerty.chaekit.global.response.ApiSuccessResponse;
 
+@Slf4j
 @RestController
 public class MainController {
     @GetMapping("/api")
     public ApiSuccessResponse<String> mainApi() {
+        log.info("Health check API called");
         return ApiSuccessResponse.of("Welcome to Chaekit");
     }
 }
