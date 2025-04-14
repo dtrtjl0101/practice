1e5244a202faa7c508075bb13269bb337019ded3
bigfoodk <satcom1600@hanmail.net>
2025-04-14 20:43:27 +0900

    Construct workflow

--- client/cdk/lib/cdk-stack.ts
+++ client/cdk/lib/cdk-stack.ts
@@ -1,16 +1,108 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // ==============================================================
+    const bucketName = new cdk.CfnParameter(this, "bucketName", {
+      type: "String",
+      description: "S3 bucket name for client static files",
+    });
+    const domainName = new cdk.CfnParameter(this, "domainName", {
+      type: "String",
+      description: "Domain name for client",
+    });
+    const subDomainName = new cdk.CfnParameter(this, "subDomainName", {
+      type: "String",
+      description: "Subdomain name for client",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // ==============================================================
+    const bucket = new cdk.aws_s3.Bucket(this, "ClientBucket", {
+      bucketName: bucketName.valueAsString,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      publicReadAccess: false,
+    });
+
+    // ==============================================================
+    new cdk.aws_s3_deployment.BucketDeployment(this, "DeployClient", {
+      sources: [cdk.aws_s3_deployment.Source.asset("../dist")],
+      destinationBucket: bucket,
+    });
+
+    // ==============================================================
+    const clientDomainName =
+      subDomainName.valueAsString === "master"
+        ? domainName.valueAsString
+        : `${subDomainName.valueAsString}.${domainName.valueAsString}`;
+    const zone = cdk.aws_route53.HostedZone.fromLookup(this, "Zone", {
+      domainName: domainName.valueAsString,
+    });
+
+    const certificate = new cdk.aws_certificatemanager.Certificate(
+      this,
+      "Certificate",
+      {
+        domainName: clientDomainName,
+        validation:
+          cdk.aws_certificatemanager.CertificateValidation.fromDns(zone),
+      }
+    );
+
+    // ==============================================================
+    const originAccessIdentity = new cdk.aws_cloudfront.OriginAccessIdentity(
+      this,
+      "OriginAccessIdentity"
+    );
+    const s3BucketOrigin =
+      cdk.aws_cloudfront_origins.S3BucketOrigin.withOriginAccessIdentity(
+        bucket,
+        {
+          originAccessIdentity,
+        }
+      );
+    const cloudFront = new cdk.aws_cloudfront.Distribution(
+      this,
+      "ClientCloudFront",
+      {
+        defaultRootObject: "index.html",
+        defaultBehavior: {
+          origin: s3BucketOrigin,
+        },
+        errorResponses: [
+          {
+            httpStatus: 403,
+            responseHttpStatus: 200,
+            responsePagePath: "/",
+            ttl: cdk.Duration.seconds(0),
+          },
+          {
+            httpStatus: 404,
+            responseHttpStatus: 200,
+            responsePagePath: "/",
+            ttl: cdk.Duration.seconds(0),
+          },
+        ],
+        certificate,
+        domainNames: [clientDomainName],
+      }
+    );
+
+    // ==============================================================
+    new cdk.aws_route53.ARecord(this, "ClientRecord", {
+      zone,
+      recordName: clientDomainName,
+      target: cdk.aws_route53.RecordTarget.fromAlias(
+        new cdk.aws_route53_targets.CloudFrontTarget(cloudFront)
+      ),
+    });
+
+    const clientDomain = new cdk.CfnOutput(this, "client url", {
+      value: `https://${clientDomainName}`,
+    });
   }
 }
