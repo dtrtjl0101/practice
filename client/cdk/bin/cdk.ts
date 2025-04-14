3778a9e49a085120b176904d6eabdd3b47e4befe
bigfoodk <satcom1600@hanmail.net>
2025-04-15 02:22:40 +0900

    Fix stack name

--- client/cdk/bin/cdk.ts
+++ client/cdk/bin/cdk.ts
@@ -5,14 +5,14 @@ import { ClientDeploymentStack } from "../lib/ClientDeploymentStack";
 
 const app = new cdk.App();
 const certificationStack = new CertificationStack(app, "CertificationStack", {
-  stackName: `${process.env.CDK_STACK_NAME}CertificationStack`,
+  stackName: `${process.env.STACK_NAME}CertificationStack`,
   env: {
     region: "us-east-1",
   },
 });
 new ClientDeploymentStack(app, "ClientDeploymentStack", {
   certificate: certificationStack.certificate,
-  stackName: `${process.env.CDK_STACK_NAME}ClientDeploymentStack`,
+  stackName: `${process.env.STACK_NAME}ClientDeploymentStack`,
   crossRegionReferences: true,
   env: {
     region: process.env.AWS_DEFAULT_REGION,
