11898e8e706d77d22d619a05bc05c053e46a003e
고종환 <jonghwankoh@ajou.ac.kr>
2025-04-16 02:22:40 +0900

    fix: 일관성 없는 응답형식 수정(GroupPostResponse

--- chaekit-spring/src/main/java/qwerty/chaekit/dto/group/GroupPostResponse.java
+++ chaekit-spring/src/main/java/qwerty/chaekit/dto/group/GroupPostResponse.java
@@ -2,12 +2,25 @@ package qwerty.chaekit.dto.group;
 
 import lombok.Builder;
 import qwerty.chaekit.domain.group.ReadingGroup;
+import qwerty.chaekit.domain.group.tag.GroupTag;
+
+import java.util.List;
 
 @Builder
 public record GroupPostResponse(
-    GroupFetchResponse group
+        Long groupId,
+        String name,
+        String description,
+        List<String> tags
 ) {
     public static GroupPostResponse of(ReadingGroup group) {
-        return new GroupPostResponse(GroupFetchResponse.of(group));
+        return GroupPostResponse.builder()
+                .groupId(group.getId())
+                .name(group.getName())
+                .description(group.getDescription())
+                .tags(group.getTags().stream()
+                        .map(GroupTag::getTagName)
+                        .toList())
+                .build();
     }
 }
