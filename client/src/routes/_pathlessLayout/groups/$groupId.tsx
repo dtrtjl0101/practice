fdab9ef7d07f2f212e013486070004d8c43ab24a
bigfoodk <satcom1600@hanmail.net>
2025-04-17 20:23:00 +0900

    Show activities of group

--- client/src/routes/_pathlessLayout/groups/$groupId.tsx
+++ client/src/routes/_pathlessLayout/groups/$groupId.tsx
@@ -2,19 +2,23 @@ import { createFileRoute } from "@tanstack/react-router";
 import { GroupInfo } from "../../../types/groups";
 import {
   Box,
+  CardMedia,
   Chip,
   Container,
   Divider,
   Grid2,
   Icon,
+  Pagination,
   Paper,
   Skeleton,
   Stack,
   Typography,
 } from "@mui/material";
-import { People } from "@mui/icons-material";
+import { People, Timelapse } from "@mui/icons-material";
 import { useQuery } from "@tanstack/react-query";
 import API_CLIENT, { wrapApiResponse } from "../../../api/api";
+import { useState } from "react";
+import { Activity } from "../../../types/activity";
 
 export const Route = createFileRoute("/_pathlessLayout/groups/$groupId")({
   component: RouteComponent,
@@ -98,15 +102,7 @@ function RouteComponent() {
             </Grid2>
           </Stack>
         </Paper>
-        <Paper sx={{ p: 2 }}>
-          <Stack spacing={2}>
-            <Typography variant="h4">현재 활동</Typography>
-            <Divider />
-            <Typography variant="body1" sx={{ mt: 2 }}>
-              현재활동
-            </Typography>
-          </Stack>
-        </Paper>
+        <ActivityCard groupId={groupId} />
         <Paper sx={{ p: 2 }}>
           <Stack spacing={2}>
             <Typography variant="h4">모임 대화방</Typography>
@@ -118,5 +114,122 @@ function RouteComponent() {
         </Paper>
       </Stack>
     </Container>
+  );
+}
+
+function ActivityCard(props: { groupId: string }) {
+  const { groupId } = props;
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const { data: activity, error } = useQuery({
+    queryKey: ["activity", groupId, page],
+    queryFn: async () => {
+      const groupIdNumber = parseInt(groupId);
+      if (isNaN(groupIdNumber)) {
+        throw new Error("INVALID_GROUP_ID");
+      }
+      const response = await wrapApiResponse(
+        API_CLIENT.activityController.getAllActivities(groupIdNumber, {
+          page,
+          size: 1,
+        })
+      );
+
+      if (!response.isSuccessful) {
+        console.error(response.errorMessage);
+        throw new Error(response.errorCode);
+      }
+
+      setTotalPages(response.data.totalPages!);
+
+      const activity = response.data.content![0] as Activity | undefined;
+      if (!activity) {
+        throw new Error("NO_ACTIVITY");
+      }
+
+      return activity;
+    },
+  });
+
+  const noActivity = error && error.message === "NO_ACTIVITY";
+
+  return (
+    <Paper sx={{ p: 2 }}>
+      <Stack spacing={2}>
+        <Typography variant="h4">활동</Typography>
+        <Divider />
+        {noActivity ? (
+          <Typography variant="body1" sx={{ mt: 2 }} color="textSecondary">
+            아직 활동이 없어요
+          </Typography>
+        ) : (
+          <Stack spacing={2} direction={"row"}>
+            <CardMedia
+              // TODO: Use book image
+              image="https://picsum.photos/256/256"
+              sx={{
+                width: 256,
+                height: 256,
+                borderRadius: 2,
+              }}
+            />
+            <Stack sx={{ flexGrow: 1 }}>
+              <Stack>
+                {activity ? (
+                  <Typography variant="h5">{activity.bookId}</Typography>
+                ) : (
+                  <Skeleton height={48} />
+                )}
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "flex-end",
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Icon>
+                      <Timelapse />
+                    </Icon>
+                    {activity ? (
+                      `${new Date(activity.startTime).toLocaleDateString()} ~
+                    ${new Date(activity.endTime).toLocaleDateString()}`
+                    ) : (
+                      <Skeleton width={256} />
+                    )}
+                  </Typography>
+                </Box>
+              </Stack>
+              <Divider />
+              {activity ? (
+                <Typography variant="body1" sx={{ mt: 2 }}>
+                  {activity.description}
+                </Typography>
+              ) : (
+                <Skeleton sx={{ mt: 2 }} />
+              )}
+            </Stack>
+          </Stack>
+        )}
+        <Divider />
+        <Pagination
+          page={page + 1}
+          count={totalPages}
+          onChange={(_, page) => {
+            setPage(page - 1);
+          }}
+          sx={{ width: "100%", justifyItems: "center" }}
+        />
+      </Stack>
+    </Paper>
   );
 }
---
+++ client/src/types/activity.ts
@@ -0,0 +1,7 @@
+export type Activity = {
+  activityId: number;
+  bookId: number;
+  startTime: string;
+  endTime: string;
+  description: string;
+};
