04df4028c5103d8f3e9d723ee43c3274a0ad37a9
bigfoodk <satcom1600@hanmail.net>
2025-04-21 20:09:29 +0900

    Add more nav buttons

--- client/src/component/AppBar.tsx
+++ client/src/component/AppBar.tsx
@@ -1,4 +1,4 @@
-import { Toolbar, AppBar as MuiAppBar, Button } from "@mui/material";
+import { Toolbar, AppBar as MuiAppBar, Button, Divider } from "@mui/material";
 import { createLink, Link, LinkComponentProps } from "@tanstack/react-router";
 import { useAtomValue } from "jotai";
 import { AuthState } from "../states/auth";
@@ -16,12 +16,14 @@ export default function AppBar() {
         <LogoButton to="/" sx={{ mr: "auto" }}>
           <img src="/logoTitle.png" alt="Logo" height={40} />
         </LogoButton>
+        <LinkNavButton to={"/books"} text="도서" />
+        <LinkNavButton to={"/groups"} text="모임" />
+        <Divider orientation="vertical" flexItem variant="middle" />
         {user && user.role === Role.ROLE_ADMIN && (
           <LinkNavButton to={"/admin"} text="관리자" />
         )}
         {user ? (
           <>
-            <LinkNavButton to={"/books"} text="도서" />
             <LinkNavButton to={"/mypage"} text="마이페이지" />
             <Button onClick={logout}>로그아웃</Button>
           </>
@@ -41,7 +43,12 @@ function LinkNavButton(props: { to: LinkComponentProps["to"]; text: string }) {
     <Link to={to}>
       {({ isActive }) => {
         return (
-          <Button color={isActive ? "primary" : "secondary"}>{text}</Button>
+          <Button
+            color={isActive ? "primary" : "secondary"}
+            sx={{ fontWeight: isActive ? "bold" : "regular" }}
+          >
+            {text}
+          </Button>
         );
       }}
     </Link>
