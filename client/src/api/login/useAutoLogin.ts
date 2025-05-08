37f8f971e39eba85b3838476d216bc859ca785de
bigfoodk <satcom1600@hanmail.net>
2025-05-08 16:52:36 +0900

    Refactor getLoggedInUserFromLocalStorage

--- client/src/api/login/useAutoLogin.ts
+++ client/src/api/login/useAutoLogin.ts
@@ -1,22 +1,14 @@
 import { useEffect } from "react";
-import { AuthState } from "../../states/auth";
 import useLogin from "./useLogin";
+import getLoggedInUserFromLocalStorage from "../../util/getLoggedInUserFromLocalStorage";
 
 export default function useAutoLogin() {
   const { login } = useLogin();
   useEffect(() => {
-    try {
-      const savedLoggedInUser = localStorage.getItem("loggedInUser");
-      if (!savedLoggedInUser) {
-        return;
-      }
-      const parsedLoggedInUser: AuthState.LoggedInUser =
-        JSON.parse(savedLoggedInUser);
-      login(parsedLoggedInUser);
-    } catch (e) {
-      console.error("Error parsing loggedInUser from localStorage", e);
-      localStorage.removeItem("loggedInUser");
-      return undefined;
+    const savedLoggedInUser = getLoggedInUserFromLocalStorage();
+    if (!savedLoggedInUser) {
+      return;
     }
+    login(savedLoggedInUser);
   }, []);
 }
