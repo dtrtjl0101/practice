c92c4f2ad7e9a4a44896df79918a2774496e88f6
bigfoodk <satcom1600@hanmail.net>
2025-05-22 14:41:36 +0900

    Show members on group page

--- client/src/component/_pathlessLayout/groups/$groupId/GroupHeader.tsx
+++ client/src/component/_pathlessLayout/groups/$groupId/GroupHeader.tsx
@@ -9,13 +9,19 @@ import {
   Divider,
   Grid,
   Chip,
+  Popover,
+  Card,
+  Avatar,
+  Box,
 } from "@mui/material";
-import { People, Settings } from "@mui/icons-material";
+import { Group, Settings } from "@mui/icons-material";
 import { LinkIconButton } from "../../../../component/_pathlessLayout/groups/$groupId/LinkIconButton";
 import { GroupInfo, GroupMembershipStatus } from "../../../../types/groups";
 import API_CLIENT from "../../../../api/api";
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
+const MEMBERS_SIZE = 2;
 interface GroupHeaderProps {
   group: GroupInfo | undefined;
   groupId: number;
@@ -23,6 +29,28 @@ interface GroupHeaderProps {
 
 export function GroupHeader({ group, groupId }: GroupHeaderProps) {
   const [joinGroupRequested, setJoinGroupRequested] = useState(false);
+  const [membersPopoverAnchorElement, setMembersPopoverAnchorElement] =
+    useState<HTMLButtonElement | null>(null);
+  const [hasMoreMembers, setHasMoreMembers] = useState(false);
+
+  const { data: members } = useQuery({
+    queryKey: ["groupMembers", groupId],
+    queryFn: async () => {
+      const response = await API_CLIENT.groupController.getGroupMembers(
+        groupId,
+        {
+          page: 0,
+          size: MEMBERS_SIZE,
+        }
+      );
+      if (!response.isSuccessful) {
+        throw new Error(response.errorMessage);
+      }
+      setHasMoreMembers((response.data.totalItems || 0) > MEMBERS_SIZE);
+      return response.data.content;
+    },
+    initialData: [],
+  });
 
   const onJoinButtonClicked = async () => {
     const response = await API_CLIENT.groupController.requestJoinGroup(groupId);
@@ -46,117 +74,174 @@ export function GroupHeader({ group, groupId }: GroupHeaderProps) {
     };
   };
 
+  const onMembersButtonClicked = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setMembersPopoverAnchorElement(event.currentTarget);
+  };
+  const handleProgressPopoverClose = () => {
+    setMembersPopoverAnchorElement(null);
+  };
+
   if (!group) {
     return <GroupHeaderSkeleton />;
   }
 
   return (
-    <Paper sx={{ p: 2 }}>
-      <Stack spacing={2}>
-        <Stack direction={"row"} spacing={2}>
-          <Stack flexGrow={1}>
-            <Stack direction={"row"} spacing={2} flexGrow={1}>
-              <Stack>
-                {group ? (
-                  <CardMedia
-                    image={group.groupImageURL}
-                    sx={{ width: 256, height: 256 }}
-                  />
-                ) : (
-                  <Skeleton variant="rectangular" width={256} height={256} />
-                )}
-              </Stack>
-              <Stack sx={{ flexGrow: 1 }}>
-                <Stack direction={"row"} spacing={1}>
-                  <Typography variant="h3" flexGrow={1}>
-                    {group ? group.name : <Skeleton variant="text" />}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    sx={{
-                      display: "flex",
-                      alignItems: "center",
-                    }}
+    <>
+      <Popover
+        open={!!membersPopoverAnchorElement}
+        anchorEl={membersPopoverAnchorElement}
+        onClose={handleProgressPopoverClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        disableRestoreFocus
+        slotProps={{
+          paper: {
+            sx: { p: 2, minWidth: 320 },
+          },
+        }}
+      >
+        <Typography variant="subtitle1" sx={{ mb: 1 }}>
+          모임 멤버
+        </Typography>
+        <Divider />
+        {members && members.length === 0 ? (
+          <Typography variant="body2">아직 멤버가 없습니다.</Typography>
+        ) : (
+          <Stack spacing={1} textAlign={"center"}>
+            {members &&
+              members.map((member) => (
+                <Card key={member.userId}>
+                  <Stack
+                    direction={"row"}
+                    spacing={1}
+                    alignItems={"center"}
+                    sx={{ p: 2 }}
                   >
-                    <Icon>
-                      <People />
-                    </Icon>
-                    {group ? group.memberCount : <Skeleton />}
-                  </Typography>
-                  {group.myMemberShipStatus === GroupMembershipStatus.OWNED && (
-                    <LinkIconButton
-                      to={"/groups/$groupId/manage"}
-                      params={{ groupId }}
-                      size="large"
-                      sx={{
-                        alignSelf: "center",
-                        justifySelf: "flex-end",
-                      }}
-                    >
-                      <Settings />
-                    </LinkIconButton>
+                    <Avatar src={member.profileImageURL} />
+                    <Typography variant="body2">{member.nickname!}</Typography>
+                  </Stack>
+                </Card>
+              ))}
+            {hasMoreMembers && (
+              <Box>
+                <Typography variant="body2" color="textSecondary">
+                  {`+ ${group.memberCount - (members?.length || 0)}`}
+                </Typography>
+              </Box>
+            )}
+          </Stack>
+        )}
+      </Popover>
+      <Paper sx={{ p: 2 }}>
+        <Stack spacing={2}>
+          <Stack direction={"row"} spacing={2}>
+            <Stack flexGrow={1}>
+              <Stack direction={"row"} spacing={2} flexGrow={1}>
+                <Stack>
+                  {group ? (
+                    <CardMedia
+                      image={group.groupImageURL}
+                      sx={{ width: 256, height: 256 }}
+                    />
+                  ) : (
+                    <Skeleton variant="rectangular" width={256} height={256} />
                   )}
                 </Stack>
-                <Typography variant="body1" flexGrow={1}>
-                  {group ? group.description : <Skeleton />}
-                </Typography>
-                {(group.myMemberShipStatus === GroupMembershipStatus.NONE ||
-                  group.myMemberShipStatus ===
-                    GroupMembershipStatus.PENDING) && (
-                  <Button
-                    onClick={onJoinButtonClicked}
-                    variant="contained"
-                    disabled={joinGroupRequested}
-                    sx={{
-                      justifySelf: "flex-end",
-                      alignSelf: "flex-end",
-                      width: "fit-content",
-                    }}
-                  >
-                    {group.myMemberShipStatus === GroupMembershipStatus.PENDING
-                      ? "가입 대기중"
-                      : "가입하기"}
-                  </Button>
-                )}
-                {group.myMemberShipStatus === GroupMembershipStatus.JOINED && (
-                  <Button
-                    variant="contained"
-                    color="error"
-                    onClick={onLeaveButtonClicked()}
-                    sx={{
-                      justifySelf: "flex-end",
-                      alignSelf: "flex-end",
-                      width: "fit-content",
-                    }}
-                  >
-                    모임 탈퇴하기
-                  </Button>
-                )}
+                <Stack sx={{ flexGrow: 1 }}>
+                  <Stack direction={"row"} spacing={1}>
+                    <Typography variant="h3" flexGrow={1}>
+                      {group ? group.name : <Skeleton variant="text" />}
+                    </Typography>
+                    <Button
+                      sx={{ ml: "auto", gap: 0.5 }}
+                      onClick={onMembersButtonClicked}
+                    >
+                      <Icon>
+                        <Group />
+                      </Icon>
+                      {group.memberCount}
+                    </Button>
+                    {group.myMemberShipStatus ===
+                      GroupMembershipStatus.OWNED && (
+                      <LinkIconButton
+                        to={"/groups/$groupId/manage"}
+                        params={{ groupId }}
+                        size="large"
+                        sx={{
+                          alignSelf: "center",
+                          justifySelf: "flex-end",
+                        }}
+                      >
+                        <Settings />
+                      </LinkIconButton>
+                    )}
+                  </Stack>
+                  <Typography variant="body1" flexGrow={1}>
+                    {group ? group.description : <Skeleton />}
+                  </Typography>
+                  {(group.myMemberShipStatus === GroupMembershipStatus.NONE ||
+                    group.myMemberShipStatus ===
+                      GroupMembershipStatus.PENDING) && (
+                    <Button
+                      onClick={onJoinButtonClicked}
+                      variant="contained"
+                      disabled={joinGroupRequested}
+                      sx={{
+                        justifySelf: "flex-end",
+                        alignSelf: "flex-end",
+                        width: "fit-content",
+                      }}
+                    >
+                      {group.myMemberShipStatus ===
+                      GroupMembershipStatus.PENDING
+                        ? "가입 대기중"
+                        : "가입하기"}
+                    </Button>
+                  )}
+                  {group.myMemberShipStatus ===
+                    GroupMembershipStatus.JOINED && (
+                    <Button
+                      variant="contained"
+                      color="error"
+                      onClick={onLeaveButtonClicked()}
+                      sx={{
+                        justifySelf: "flex-end",
+                        alignSelf: "flex-end",
+                        width: "fit-content",
+                      }}
+                    >
+                      모임 탈퇴하기
+                    </Button>
+                  )}
+                </Stack>
               </Stack>
             </Stack>
           </Stack>
+          <Divider />
+          <Grid container spacing={1}>
+            {group ? (
+              group.tags.map((tag) => {
+                return (
+                  <Chip
+                    key={tag}
+                    label={tag}
+                    onClick={() => {
+                      // TODO: move to search page with tag
+                    }}
+                  />
+                );
+              })
+            ) : (
+              <Skeleton width={128} />
+            )}
+          </Grid>
         </Stack>
-        <Divider />
-        <Grid container spacing={1}>
-          {group ? (
-            group.tags.map((tag) => {
-              return (
-                <Chip
-                  key={tag}
-                  label={tag}
-                  onClick={() => {
-                    // TODO: move to search page with tag
-                  }}
-                />
-              );
-            })
-          ) : (
-            <Skeleton width={128} />
-          )}
-        </Grid>
-      </Stack>
-    </Paper>
+      </Paper>
+    </>
   );
 }
 function GroupHeaderSkeleton() {
