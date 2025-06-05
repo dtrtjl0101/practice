1d14a8c4daadceb176a21b37909303e1df706236
bigfoodk <satcom1600@hanmail.net>
2025-06-05 18:49:27 +0900

    Disable coachmark in home page

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -17,7 +17,6 @@ import {
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import BookList from "../component/BookList";
 import EventCarousel from "../component/EventCarousel";
-import Coachmark, { useCoachmark } from "../component/Coachmark";
 import {
   Groups,
   MenuBook,
@@ -601,7 +600,6 @@ function HowItWorksSection() {
 // Main Home Component
 function Home() {
   const navigate = useNavigate();
-  const { isOpen, completeCoachmark } = useCoachmark("home");
   const theme = useTheme();
 
   return (
@@ -699,32 +697,6 @@ function Home() {
           </Stack>
         </Paper>
       </Stack>
-      <Coachmark
-        steps={[
-          {
-            target: ".coachmark-event-carousel",
-            title: "환영합니다! 📚",
-            content: "여기서 최신 독서 이벤트와 공지사항을 확인할 수 있습니다.",
-            placement: "bottom",
-          },
-          {
-            target: ".coachmark-popular-groups",
-            title: "인기 모임 🏆",
-            content:
-              "다른 독서 애호가들과 함께 책을 읽고 토론할 수 있는 인기 모임들입니다. '더보기'를 클릭해서 더 많은 모임을 둘러보세요!",
-            placement: "bottom",
-          },
-          {
-            target: ".coachmark-popular-books",
-            title: "인기 도서 📖",
-            content:
-              "지금 가장 인기 있는 책들을 확인하고, 새로운 읽을거리를 찾아보세요.",
-            placement: "top",
-          },
-        ]}
-        isOpen={isOpen}
-        onComplete={completeCoachmark}
-      />
     </Container>
   );
 }
