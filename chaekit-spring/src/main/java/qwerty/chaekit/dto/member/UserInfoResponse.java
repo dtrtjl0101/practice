d210dec36013f49edc145590930f64cdde965ac4
고종환 <jonghwankoh@ajou.ac.kr>
2025-06-10 01:57:55 +0900

    [FIX] UserInfoResponse에 로그인할 때 나오는 정보 추가

--- chaekit-spring/src/main/java/qwerty/chaekit/dto/member/UserInfoResponse.java
+++ chaekit-spring/src/main/java/qwerty/chaekit/dto/member/UserInfoResponse.java
@@ -5,15 +5,21 @@ import qwerty.chaekit.domain.member.user.UserProfile;
 
 @Builder
 public record UserInfoResponse(
+        Long memberId,
+        String email,
         Long userId,
         String nickname,
-        String profileImageURL
+        String profileImageURL,
+        String role
 ){
     public static UserInfoResponse of(UserProfile user, String profileImageURL) {
         return UserInfoResponse.builder()
+                .memberId(user.getMember().getId())
+                .email(user.getMember().getEmail())
                 .userId(user.getId())
                 .nickname(user.getNickname())
                 .profileImageURL(profileImageURL)
+                .role(user.getMember().getRole().name())
                 .build();
     }
 }
