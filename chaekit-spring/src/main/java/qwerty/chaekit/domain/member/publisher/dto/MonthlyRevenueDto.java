be693250f41c3addf4d843f7e8491690e1ba58f4
dtrtjl0101 <dtrtjl0101@naver.com>
2025-07-17 20:18:54 +0900

    delete controller

--- chaekit-spring/src/main/java/qwerty/chaekit/domain/member/publisher/dto/MonthlyRevenueDto.java
+++ chaekit-spring/src/main/java/qwerty/chaekit/domain/member/publisher/dto/MonthlyRevenueDto.java
@@ -1,11 +1,11 @@
-package qwerty.chaekit.domain.member.publisher.dto;
-
-public record MonthlyRevenueDto(
-        Long totalSalesCount,
-        Long totalRevenue,
-        Long totalActivityCount,
-        Long totalViewCount,
-        Long increasedSalesCount,
-        Long increasedRevenue,
-        Long increasedActivityCount
-) { }
+//package qwerty.chaekit.domain.member.publisher.dto;
+//
+//public record MonthlyRevenueDto(
+//        Long totalSalesCount,
+//        Long totalRevenue,
+//        Long totalActivityCount,
+//        Long totalViewCount,
+//        Long increasedSalesCount,
+//        Long increasedRevenue,
+//        Long increasedActivityCount
+//) { }
