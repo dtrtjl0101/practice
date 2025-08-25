2acb9c0cc2a303c7e8a973b91913418e4896bc0d
dtrtjl0101 <dtrtjl0101@naver.com>
2025-08-25 19:25:09 +0900

    cloudformation수동수정4

--- client/cdk/lib/ClientDeploymentStack.ts
+++ client/cdk/lib/ClientDeploymentStack.ts
@@ -1,19 +1,26 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as route53_targets from "aws-cdk-lib/aws-route53-targets";
+import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as s3_deployment from "aws-cdk-lib/aws-s3-deployment";
+import * as iam from "aws-cdk-lib/aws-iam";
 
 export class ClientDeploymentStack extends cdk.Stack {
   constructor(
     scope: Construct,
     id: string,
     props: cdk.StackProps & {
-      certificate: cdk.aws_certificatemanager.ICertificate;
+      certificate: acm.ICertificate;
     }
   ) {
     super(scope, id, props);
+
     const { certificate } = props;
 
-    // ==============================================================
     const bucketName = `${process.env.BRANCH_NAME}-chaekit-client`;
     const domainName = `${process.env.DOMAIN_NAME}`;
     const subDomainName = `${process.env.BRANCH_NAME}`;
@@ -21,95 +28,67 @@ export class ClientDeploymentStack extends cdk.Stack {
       subDomainName === "main" ? domainName : `${subDomainName}.${domainName}`;
     const zoneId = `${process.env.ZONE_ID}`;
 
-    // ==============================================================
-    const bucket = new cdk.aws_s3.Bucket(this, "ClientBucket", {
-      bucketName: bucketName,
+    // S3 버킷 생성
+    const bucket = new s3.Bucket(this, "ClientBucket", {
+      bucketName,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
       publicReadAccess: false,
     });
 
-    // ==============================================================
-    const zone = cdk.aws_route53.HostedZone.fromHostedZoneAttributes(
-      this,
-      "Zone",
-      {
-        hostedZoneId: zoneId,
-        zoneName: domainName,
-      }
-    );
+    // Route53 Hosted Zone
+    const zone = route53.HostedZone.fromHostedZoneAttributes(this, "Zone", {
+      hostedZoneId: zoneId,
+      zoneName: domainName,
+    });
 
-    // ==============================================================
-    const originAccessIdentity = new cdk.aws_cloudfront.OriginAccessIdentity(
+    // CloudFront OAI (Origin Access Identity)
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(
       this,
       "OriginAccessIdentity"
     );
-    const s3BucketOrigin =
-      cdk.aws_cloudfront_origins.S3BucketOrigin.withOriginAccessIdentity(
-        bucket,
-        {
-          originAccessIdentity,
-        }
-      );
-    const cloudFront = new cdk.aws_cloudfront.Distribution(
-      this,
-      "ClientCloudFront",
-      {
-        defaultRootObject: "index.html",
-        defaultBehavior: {
-          origin: s3BucketOrigin,
-        },
-        errorResponses: [
-          {
-            httpStatus: 403,
-            responseHttpStatus: 200,
-            responsePagePath: "/",
-            ttl: cdk.Duration.seconds(0),
-          },
-          {
-            httpStatus: 404,
-            responseHttpStatus: 200,
-            responsePagePath: "/",
-            ttl: cdk.Duration.seconds(0),
-          },
-        ],
-        certificate,
-        domainNames: [clientDomainName],
-      }
-    );
 
-    // ==============================================================
-    const deployment = new cdk.aws_s3_deployment.BucketDeployment(
-      this,
-      "DeployClient",
-      {
-        sources: [cdk.aws_s3_deployment.Source.asset("../dist")],
-        destinationBucket: bucket,
-        distribution: cloudFront,
-        distributionPaths: ["/*"],
-      }
-    );
+    // S3Origin 생성 (CDK v2)
+    const s3BucketOrigin = new S3Origin(bucket, { originAccessIdentity });
+
+    // CloudFront 배포
+    const cloudFront = new cloudfront.Distribution(this, "ClientCloudFront", {
+      defaultRootObject: "index.html",
+      defaultBehavior: {
+        origin: s3BucketOrigin,
+      },
+      errorResponses: [
+        { httpStatus: 403, responseHttpStatus: 200, responsePagePath: "/", ttl: cdk.Duration.seconds(0) },
+        { httpStatus: 404, responseHttpStatus: 200, responsePagePath: "/", ttl: cdk.Duration.seconds(0) },
+      ],
+      certificate, // us-east-1 ACM 인증서
+      domainNames: [clientDomainName],
+    });
+
+    // S3 배포
+    const deployment = new s3_deployment.BucketDeployment(this, "DeployClient", {
+      sources: [s3_deployment.Source.asset("../dist")],
+      destinationBucket: bucket,
+      distribution: cloudFront,
+      distributionPaths: ["/*"],
+    });
+
     deployment.handlerRole.addToPrincipalPolicy(
-      new cdk.aws_iam.PolicyStatement({
-        effect: cdk.aws_iam.Effect.ALLOW,
-        actions: [
-          "cloudFront:GetInvalidation",
-          "cloudFront:CreateInvalidation",
-        ],
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["cloudfront:GetInvalidation", "cloudfront:CreateInvalidation"],
         resources: ["*"],
       })
     );
 
-    // ==============================================================
-    new cdk.aws_route53.ARecord(this, "ClientRecord", {
+    // Route53 A 레코드 생성
+    new route53.ARecord(this, "ClientRecord", {
       zone,
       recordName: clientDomainName,
-      target: cdk.aws_route53.RecordTarget.fromAlias(
-        new cdk.aws_route53_targets.CloudFrontTarget(cloudFront)
-      ),
+      target: route53.RecordTarget.fromAlias(new route53_targets.CloudFrontTarget(cloudFront)),
     });
 
-    new cdk.CfnOutput(this, "Client url", {
+    new cdk.CfnOutput(this, "ClientURL", {
       value: `https://${clientDomainName}`,
     });
   }
